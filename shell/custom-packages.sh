1c527e9d64a9b0327d40349a696de7e318fa4611
Alon <1578465250@qq.com>
2026-09-11 19:50:46 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -51,7 +51,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-dufs-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES openssh-sftp-server"
 # usb无线网卡+随身WiFi
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-3ginfo-lite-zh-cn"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES usb-modeswitch-data usb-modeswitch kmod-usb-net-rndis kmod-usb-net-cdc-ether"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES usb-modeswitch kmod-usb-net-rndis kmod-usb-net-cdc-ether"
 #
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-acl-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-acme-zh-cn"

a60a239e7e876f632d147fd8ef3c9c3c0a66a7aa
Alon <1578465250@qq.com>
2026-09-17 10:59:13 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -75,7 +75,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-veth kmod-macvlan"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-nf-nat kmod-nf-conntrack kmod-tun"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-nft-tproxy kmod-nft-nat"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES tar gzip xz-utils nftables"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES libcap libapparmo libseccomp libpam"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES libcap libapparmor libseccomp libpam"
 #
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-openlist-zh-cn"
 #
